e1363668129f24e002a1294e92ce7e30ca51ecc9
Corey Psoinos <coreypsoinos@gmail.com>
2017-07-24 23:15:38 -0400

    initial commit

---
+++ app.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require 'rest-client'
+require 'JSON'
+require 'dotenv/load'
+
+post '/:command' do
+  send_command_to_vehicle(params[:command])
+end
+
+def session_id
+  @_session_id ||= begin
+    resp = RestClient.get("https://#{ENV['SMARTSTART_SERVER']}/auth/login/#{ENV['VIPER_USERNAME']}/#{ENV['VIPER_PASSWORD']}")
+    JSON.parse(resp.body).dig("Return", "Results", "SessionID")
+  end
+end
+
+def send_command_to_vehicle(command)
+  RestClient.get("https://#{ENV['SMARTSTART_SERVER']}/device/sendcommand/1248171/#{command}?sessid=#{session_id}") do |response, request, result|
+    case response.code
+    when 200
+      JSON.parse(response.body)
+    else
+      raise
+    end
+  end
+end
